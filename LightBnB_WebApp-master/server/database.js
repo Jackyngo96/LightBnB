5e806ca8956cbb2c1a36868c412ad559b4427460
Jacky Ngo <jacky.ngo96@gmail.com>
2023-01-23 14:50:00 -0500

    refactor getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,7 +82,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log(guest_id)
+  return pool
+  .query(`
+SELECT reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+`,
+  [guest_id, limit]
+)
+  .then((res) => {
+    console.log(res.rows);
+    return res.rows;
+  })
+  .catch((err) => 
+    console.error("query error", err.stack));
 };
 exports.getAllReservations = getAllReservations;
 
@@ -98,7 +117,6 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((res) => {
-      console.log(res.rows);
       return res.rows;
     })
     .catch((err) => 
